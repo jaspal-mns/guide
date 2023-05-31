73b6f29a489987c8280a1201d6083d6a796b294b
jaspal-mns <jaspal.x.singh@mnscorp.net>
2023-05-31 22:29:45 +0100

    Commit first

--- src/main/kotlin/com/soul/GuideController.kt
+++ src/main/kotlin/com/soul/GuideController.kt
@@ -2,13 +2,13 @@ package com.soul
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
 
 @Controller("/guide")
 class GuideController {
 
     @Get(uri="/", produces=["text/plain"])
     fun index(): String {
+        System.out.println("Hello World")
         return "Example Response"
     }
 }
